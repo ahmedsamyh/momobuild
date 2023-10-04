32c2ad31a25e70f61d90bad2ada518d43115a6bf
ahmedsamyh <ahmedsamyh10@gmail.com>
2023-10-04 10:25:15 +0500

    flags

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ struct Subcmd {
   std::function<void()> func{nullptr};
 };
 
+
 int main(int argc, char *argv[]) {
   ARG();
 
@@ -15,52 +16,65 @@ int main(int argc, char *argv[]) {
 
   // flags
   bool quiet{false};
+  bool not_build{false};
+  bool executable_name_provided{false};
+
+  auto help = [&](){							
+    print("Usage: {} [flags] [config] [subcmd]\n", program);
+    print("\nConfigs: \n"
+          "    Debug (default)\n"
+          "    Release\n"
+	  "    All\n"	 
+	  
+	  "\nFlags: \n"
+	  "    /Q    - Quite mode; do not output anything\n"	
+	  "    /Rdst - Copies vcredist files to .\\redist\n"	
+	  "    /h    - Same as the help subcommand.\n"		
+	  "    /nb   - Do not build and run premake.\n"		
+	  "    /ex   - If this flag is present, the argument after the "
+	  "run subcommand is treated as the executable_name to run.\n"
+	  
+	  "\nSubcommands: \n"
+	  "    help  - Displays how to use this script.\n"
+	  "    init  - Initializes the project folder. echo     reset - "
+	  "Resets the project folder.\n"
+	  "    run   - Runs the builded program.\n"
+	  "    srun  - Runs the builded program as a new process.\n"
+	  "    dir   - Opens the directory of the builded program.\n"
+	  "    clean - Cleans the left-over things from the last build.\n"
+	  "    sln   - Opens the .sln file of the project.\n");
+  };
 
   std::vector<Subcmd> subcommands = {
-      {"help",
-       [&]() {
-         print("Usage: {} [flags] [config] [subcmd]\n", program);
-         print("\nConfigs: \n"
-               "    Debug (default)\n"
-               "    Release\n"
-               "    All\n"
-
-               "\nFlags: \n"
-               "    /Q    - Quite mode; do not output anything\n"
-               "    /Rdst - Copies vcredist files to .\\redist\n"
-               "    /h    - Same as the help subcommand.\n"
-               "    /nb   - Do not build and run premake.\n"
-               "    /ex   - If this flag is present, the argument after the "
-               "run subcommand is treated as the executable_name to run.\n"
-
-               "\nSubcommands: \n"
-               "    help  - Displays how to use this script.\n"
-               "    init  - Initializes the project folder. echo     reset - "
-               "Resets the project folder.\n"
-               "    run   - Runs the builded program.\n"
-               "    srun  - Runs the builded program as a new process.\n"
-               "    dir   - Opens the directory of the builded program.\n"
-               "    clean - Cleans the left-over things from the last build.\n"
-               "    sln   - Opens the .sln file of the project.\n");
-       }},
-      {"init", [&]() { UNIMPLEMENTED(); }},
-      {"run", [&]() { UNIMPLEMENTED(); }},
-      {"srun", [&]() { UNIMPLEMENTED(); }},
-      {"dir", [&]() { UNIMPLEMENTED(); }},
+      {"/Q",	[&]() { quiet = true; }},
+      {"/Rdst", [&]() { UNIMPLEMENTED(); }},
+      {"/h",    help},
+      {"/nb",	[&]() { not_build = true; }},
+      {"/ex",    [&]() { executable_name_provided = true; }}
+      {"help",  help},
+      {"init",	[&]() { UNIMPLEMENTED(); }},
+      {"run",	[&]() { UNIMPLEMENTED(); }},
+      {"srun",	[&]() { UNIMPLEMENTED(); }},
+      {"dir",	[&]() { UNIMPLEMENTED(); }},
       {"clean", [&]() { UNIMPLEMENTED(); }},
-      {"sln", [&]() { UNIMPLEMENTED(); }},
+      {"sln",	[&]() { UNIMPLEMENTED(); }},
   };
 
   // parse command line arguments
-  do {
-    std::string a{arg.pop_arg()};
-    for (auto &subcmd : subcommands) {
-      if (a == subcmd.name) {
-        subcmd.func();
+  if (arg){
+    do {
+      std::string a{arg.pop_arg()};
+      bool matched{false};
+      for (auto &subcmd : subcommands) {
+        if (a == subcmd.name) {
+    	matched = true;
+          subcmd.func();
+        }
       }
-    }
-
-  } while (arg);
-
+      if (!matched){
+        ERROR(FMT("Invalid subcommand or flag `{}`", a));
+      }
+    } while (arg);
+  }
   return 0;
 }
